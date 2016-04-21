86904c36564c8272537e751a29170489a9849e95
ffarzat <fabiofarzat@gmail.com>
2016-04-21 08:08:05 -0300

    Setting Environment [refs #626]

--- src/RestApi.ts
+++ src/RestApi.ts
@@ -12,4 +12,4 @@ app.model(Item);
 app.use('/api', loopback.rest());
 app.use('/explorer', explorer(app, {basePath: '/api'}));
 app.listen(8080);
-console.log('Api running');
+console.log(`Api running ${process.env.NODE_ENV} environment`);
