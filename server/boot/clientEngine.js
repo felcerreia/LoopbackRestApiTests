2adb3845bb459dc915de033d0bd4f529c40c5fb4
ffarzat <fabiofarzat@gmail.com>
2016-04-21 19:52:02 -0300

    Clientes router [refs #626]

--- server/boot/clientEngine.js
+++ server/boot/clientEngine.js
@@ -1,14 +1,14 @@
 module.exports = function(server) {
-  // Install a `/` that renders index.marko template
+  // Install a `/` router that renders index.marko template
  
   var router = server.loopback.Router();
   
   var templatePath = require.resolve('../../client/index/index.marko');
   var template = require('marko').load(templatePath);
   
-  
-  router.get('/', function (req, res) {
-      template.stream({name: 'Visitante'}).pipe(res);
+  router.get('/Clientes', function (req, res) {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      template.stream({name: 'Fabio Farzat'}).pipe(res);
     });
   
   server.use(router);
