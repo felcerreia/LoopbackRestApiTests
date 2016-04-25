42d589a303b24ce1f6c65e8f686a16035312a145
ffarzat <fabiofarzat@gmail.com>
2016-04-25 18:29:45 -0300

    Transaction sample

--- ComprevPoc/common/models/cliente.js
+++ ComprevPoc/common/models/cliente.js
@@ -1,3 +1,52 @@
-module.exports = function(Cliente) {
+module.exports = function (Cliente) {
+
+    //Registro um novo endpoint
+    Cliente.remoteMethod(
+        'inserirClienteComEndereco',
+        {
+            description: 'Insere um cliente e seus endereços atomicamente [transação]',
+            accepts: {
+                arg: 'dto',
+                type: 'inserirClienteComEndereco',
+                required: true,
+                http: {
+                    source: 'body'
+                }
+            },
+            http: { path: '/:inserirClienteComEndereco', verb: 'post' },
+            returns: { arg: 'dto', type: 'DtoInserirCliente' }
+        });
+    
+    //Agora insiro a lógica       
+    Cliente.inserirClienteComEndereco = function (dto, callback) {
+        
+        Cliente.beginTransaction({isolationLevel: Cliente.Transaction.READ_COMMITTED}, function(err, tx) {
+  
+            var tansactionObj = { transaction: tx };
+  
+            dto.cliente.create(dto.Cliente, function (err, tansactionObj, obj) {
+                if (err) throw err;
+                console.log('> Erro durante criação do cliente');
+            });
+
+            dto.Enderecos.create(dto.Enderecos, function (err, tansactionObj, obj) {
+                if (err) throw err;
+                console.log('> Erro durante criação dos endereços do cliente');
+            });
+
+            tansactionObj.commit(function (err) {
+                console.log('> Erro ao realizar o commit');
+
+                tansactionObj.rollback(function (err) {
+                    console.log('> Erro ao realizar o rollback');
+                });
+            });
+  
+            return dto; //dto com os Ids após insert
+  
+        });
+        
+        
+    }
 
 };
