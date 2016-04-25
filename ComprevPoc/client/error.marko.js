cea93a64605e95dcc9a6243e58a1f7b4a3e72e52
ffarzat <fabiofarzat@gmail.com>
2016-04-25 13:00:32 -0300

    PDF with Phantom working [very slow]

--- ComprevPoc/client/error.marko.js
+++ ComprevPoc/client/error.marko.js
@@ -7,7 +7,7 @@ function create(__helpers) {
   return function render(data, out) {
     out.w("<!DOCTYPE html> <html lang=\"en\"> <head> <title>Página de erro</title> <link href=\"http://getbootstrap.com/2.3.2/assets/css/bootstrap.css\" rel=\"stylesheet\"> <link href=\"http://getbootstrap.com/2.3.2/assets/css/bootstrap-responsive.css\" rel=\"stylesheet\"> <link href=\"http://getbootstrap.com/2.3.2/assets/css/docs.css\" rel=\"stylesheet\"> <link href=\"http://getbootstrap.com/2.3.2/assets/js/google-code-prettify/prettify.css\" rel=\"stylesheet\"> </head> <body> <div class=\"jumbotron masthead\"> <div class=\"container\"> <h1> Caramba! </h1> </div> <p> Olá " +
       escapeXml(data.name) +
-      "!, infelizmente houve erro ao processar seu pedido. Desculpe.</p> <div class=\"alert alert-danger bs-alert-old-docs\">" +
+      ", infelizmente houve erro ao processar seu pedido. Desculpe.</p> <div class=\"alert alert-danger bs-alert-old-docs\">" +
       escapeXml(data.error) +
       "</div> </div> </body> </html>");
   };
--- ComprevPoc/server/boot/pdfEngine.js
+++ ComprevPoc/server/boot/pdfEngine.js
@@ -1,22 +1,58 @@
 module.exports = function(server) {
-  var webPage = require('webpage');
-  var router = server.loopback.Router();
+  var router    =  server.loopback.Router();
+  var phantom = require('phantom');
+  var path = require('path');
+  var mime = require('mime');
   
   router.get('/Pdf', function (req, res){
-        
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        
-        try{
-            var page = webPage.create();
 
-            page.viewportSize = { width: 1920, height: 1080 };
+        //res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        try{
             
-            page.open("/Clientes", function start(status) {
-                page.render('clientes.pdf', {format: 'pdf', quality: '100'});
-                phantom.exit();
-            });    
-        }
+            phantom.create()
+                .then(instance => {
+                    phInstance = instance;
+                    return instance.createPage();
+                })
+                .then(page => {
+                    sitepage = page;
+                    return page.open('http://localhost:3000/Clientes');
+                })
+                .then(status => {
+                    console.log('result:', status);
+                    //sitepage.set("paperSize", { format: "A4", orientation: 'portrait', margin: '1cm' });
+                    //console.log('page.set');
+                    sitepage.render("D:/GitHub/LoopbackRestApiTests/ComprevPoc/client/pdfs/clientes.pdf");
+                    //console.log('page.render');
+                    sitepage.close();
+                    //console.log('page.close');
+                    phInstance.exit();
+                    console.log('phantom.exit');
+                                        
+                    var file = 'D:/GitHub/LoopbackRestApiTests/ComprevPoc/client/pdfs/clientes.pdf';
+
+                    var filename = path.basename(file);
+                    var mimetype = mime.lookup(file);
+
+                    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+                    res.setHeader('Content-type', mimetype);
+
+                    var filestream = fs.createReadStream(file);
+                    filestream.pipe(res);
+                    
+                    
+                })
+                .catch(error => {
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                    var templateErrorPath = require.resolve('../../client/error.marko');
+                    template = require('marko').load(templateErrorPath);
+                    template.stream({name: 'Visitante', error: err}).pipe(res);
+                    console.log(error);
+                    phInstance.exit();
+                });
+            }
         catch (err){
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
             var templateErrorPath = require.resolve('../../client/error.marko');
             template = require('marko').load(templateErrorPath);
             template.stream({name: 'Visitante', error: err}).pipe(res);
